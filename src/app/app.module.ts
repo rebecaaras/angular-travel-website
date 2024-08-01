f2131d8a14b9ba599244054b079206904d73225b
rebecaaras <rebecaaraujo2013@gmail.com>
2024-07-31 21:03:11 -0300

    Add basic structure to the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchSectionComponent } from './search-section/search-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchSectionComponent
   ],
   imports: [
     BrowserModule,
