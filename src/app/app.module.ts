a7a513899008b0bb3cd7922860d1a02a2fda182f
rebecaaras <rebecaaraujo2013@gmail.com>
2024-08-10 10:10:11 -0300

    Add page title and update search section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchSectionComponent } from './search-section/search-section.component';
+import { PageTitleComponent } from './page-title/page-title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchSectionComponent
+    SearchSectionComponent,
+    PageTitleComponent
   ],
   imports: [
     BrowserModule,
